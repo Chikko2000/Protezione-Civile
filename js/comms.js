d27de629bae3950728592262b8a44dc47439065b
Chikko2000 <superchikko2000@gmail.com>
2025-12-05 19:17:03 +0100

    Fetch communications from JSON and display them

--- js/comms.js
+++ js/comms.js
@@ -1,12 +1,17 @@
-[
-  {
-    "titolo": "Chiusura strada",
-    "data": "2025-12-05",
-    "messaggio": "Per lavori urgenti, la strada X sar\u00e0 chiusa dalle 14 alle 18."
-  },
-  {
-    "titolo": "Evento formazione volontari",
-    "data": "2025-12-04",
-    "messaggio": "Formazione teorica e pratica per nuovi volontari."
-  }
-]
+fetch('js/comunicazioni.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('comms');
+    data.forEach((comm, index) => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.style.animationDelay = `${index * 0.2}s`;
+      div.innerHTML = `
+        <h5 style="color:#004a99;">${comm.titolo}</h5>
+        <small>${comm.data}</small>
+        <p>${comm.messaggio}</p>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => console.error('Errore caricamento comunicazioni:', err));
